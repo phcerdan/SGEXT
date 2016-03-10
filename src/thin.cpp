f316d095e5f3296c2e8e32bbdce70dc7e3f09ac6
phcerdan <pablo.hernandez.cerdan@outlook.com>
2016-03-10 14:10:18 +1300

    Use boost::program_options.
    Add some images.
    Clean of dead code.

---
+++ src/thin.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <DGtal/base/Common.h>
+#include <DGtal/helpers/StdDefs.h>
+#include <DGtal/io/readers/ITKReader.h>
+#include <DGtal/images/ImageContainerByITKImage.h>
+#include <DGtal/images/imagesSetsUtils/SetFromImage.h>
+
+#include <DGtal/topology/SurfelAdjacency.h>
+#include <DGtal/io/boards/Board2D.h>
+#include <DGtal/topology/CubicalComplex.h>
+#include <DGtal/topology/CubicalComplexFunctions.h>
+#include <unordered_map>
+
+#include <DGtal/topology/VoxelComplex.h>
+#include <DGtal/topology/VoxelComplexFunctions.h>
+// Invert
+#include "itkInvertIntensityImageFilter.h"
+#include <DGtal/io/viewers/Viewer3D.h>
+#include <itkConstantPadImageFilter.h>
+#include <itkNumericTraits.h>
+// boost::program_options
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+using namespace DGtal;
+using namespace std;
+using namespace DGtal::Z3i;
+namespace po = boost::program_options;
+
+int main(int argc, char* const argv[]){
+
+  /*-------------- Parse command line -----------------------------*/
+  po::options_description general_opt ( "Allowed options are: " );
+  general_opt.add_options()
+    ( "help,h", "display this message." )
+    ( "input,i", po::value<std::string>()->required(), "Input vol file." )
+    ( "skel,s",  po::value<std::string>()->required(), "type of skeletonization" )
+    ( "foreground,f",  po::value<std::string>()->default_value("black"), "foreground color in binary image" )
+    ( "thresholdMin,m",  po::value<int>()->default_value(0), "threshold min (excluded) to define binary shape" )
+    ( "thresholdMax,M",  po::value<int>()->default_value(255), "threshold max (included) to define binary shape" )
+    ( "persistence,p",  po::value<int>()->default_value(0), "persistence value, implies use of persistence algorithm" )
+    ( "verbose,v",  po::bool_switch()->default_value(false), "verbose output" );
+  bool parseOK=true;
+  po::variables_map vm;
+
+  try {
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);
+  } catch(const std::exception& ex) {
+    parseOK=false;
+    trace.info()<< "Error checking program options: "<< ex.what()<< endl;
+  }
+  po::notify ( vm );
+  if (!parseOK || vm.count ( "help" ) || argc<=1 )
+  {
+    trace.info() <<
+      "Compute the Euleur Characteristic of  a vol to a 8-bit raw file."
+      " The vol file is first binarized using interval"
+      " [m,M[ thresholds and the Eucler characteristic is given from the cubical complex"<<std::endl
+      << std::endl << "Basic usage: "<<std::endl
+      << "asymThin -i <volFileName> -s <skel_string> [ -f <white> -m <minlevel> -M <maxlevel> -v ] "<<std::endl
+      << "options for skel_string = null, end, 1is, is" << std::endl
+      << general_opt << "\n";
+    return 0;
+  }
+  //Parse options
+  std::string filename = vm["input"].as<std::string>();
+  bool verbose = vm["verbose"].as<bool>();
+  int thresholdMin = vm["thresholdMin"].as<int>();
+  int thresholdMax = vm["thresholdMax"].as<int>();
+  int persistence = vm["persistence"].as<int>();
+  if (vm.count("persistence") && persistence < 0 )
+        throw po::validation_error(po::validation_error::invalid_option_value, "persitence");
+  std::string foreground = vm["foreground"].as<std::string>();
+  if (vm.count("foreground") && (!(foreground == "white" || foreground == "black")))
+        throw po::validation_error(po::validation_error::invalid_option_value, "foreground");
+  bool invert_image = (foreground == "black") ? true : false ;
+
+  std::string sk_string = vm["skel"].as<std::string>();
+  if (vm.count("skel") &&
+      (!( sk_string == "null" || sk_string == "end" ||
+         sk_string == "1is" || sk_string == "is")))
+        throw po::validation_error(po::validation_error::invalid_option_value, "skel");
+  /*-------------- End of parse -----------------------------*/
+
+  using Domain = Domain ;
+  using Image = ImageContainerByITKImage<Domain, unsigned char> ;
+
+  Image imageReader = ITKReader<Image>::importITK(filename);
+  const unsigned int Dim = 3;
+  using PixelType = unsigned char ;
+  using ItkImageType = itk::Image<PixelType, Dim> ;
+  // Pad image:
+  using PadType = itk::ConstantPadImageFilter<ItkImageType, ItkImageType> ;
+  auto padder = PadType::New();
+  ItkImageType::PixelType constantValue;
+  constantValue =(invert_image == true) ?
+    itk::NumericTraits<PixelType>::max() : itk::NumericTraits<PixelType>::min();
+  ItkImageType::SizeType padRegion;
+  for( unsigned int i = 0 ; i<Dim ; ++i)
+    padRegion[i] = 1;
+  padder->SetInput(imageReader.getITKImagePointer());
+  padder->SetPadLowerBound(padRegion);
+  padder->SetPadUpperBound(padRegion);
+  padder->SetConstant(constantValue);
+  padder->Update();
+  /*----------------*/
+
+  // Invert Filter using ITK.
+  using InverterType =
+    itk::InvertIntensityImageFilter<ItkImageType, ItkImageType> ;
+  auto inverter = InverterType::New();
+  inverter->SetInput(padder->GetOutput());
+  inverter->Update();
+  /*----------------*/
+  // Apply filters if neccesary
+  Image::ITKImagePointer handle_out = (invert_image) ?
+    Image::ITKImagePointer(inverter->GetOutput()) :
+    Image::ITKImagePointer(padder->GetOutput());
+  Image image(handle_out);
+
+  DigitalSet image_set (image.domain());
+  SetFromImage<Z3i::DigitalSet>::append<Image>(
+      image_set, image,
+      thresholdMin, thresholdMax);
+
+
+  // Create a VoxelComplex from the set
+
+  using DigitalTopology = DT26_6;
+  using DigitalSet =
+    DGtal::DigitalSetByAssociativeContainer<Domain ,
+      std::unordered_set< typename Domain::Point> >;
+  using Object =
+    DGtal::Object<DigitalTopology, DigitalSet>;
+  using Complex =
+    DGtal::VoxelComplex<KSpace, Object>;
+
+  KSpace ks;
+  ks.init(image.domain().lowerBound() ,
+      image.domain().upperBound() , true);
+
+  DigitalTopology::ForegroundAdjacency adjF;
+  DigitalTopology::BackgroundAdjacency adjB;
+  DigitalTopology topo(adjF, adjB, DGtal::DigitalTopologyProperties::JORDAN_DT);
+  Object obj(topo,image_set);
+
+  Complex vc(ks);
+  vc.construct(obj);
+
+  using namespace DGtal::functions ;
+
+  std::function< bool(const Complex&, const Cell&) > Skel ;
+  auto & sk = sk_string;
+  if (sk == "null") Skel = skelNull<Complex>;
+  else if (sk == "end") Skel = skelEnd<Complex>;
+  else if (sk == "1is") Skel = oneIshtmus<Complex>;
+  else if (sk == "is") Skel = skelIshtmus<Complex>;
+  else throw std::runtime_error("Invalid skel string");
+  Complex vc_new(ks);
+  if (persistence == 0)
+    vc_new = asymetricThinningScheme< Complex >(vc, selectRandom<Complex>,  Skel, verbose);
+  else
+    vc_new = persistenceAsymetricThinningScheme< Complex >(vc, selectRandom<Complex>,  Skel, persistence, verbose);
+
+  // THEN( "visualize the cells" )
+  {
+    int argc(1);
+    char** argv(nullptr);
+    QApplication app(argc, argv);
+    Viewer3D<> viewer(ks);
+    viewer.show();
+
+    viewer.setFillColor(Color(255, 255, 255, 255));
+    for ( auto it = vc_new.begin(3); it!= vc_new.end(3); ++it )
+      viewer << it->first;
+    // viewer << image_set;
+
+    // All kspace voxels
+    viewer.setFillColor(Color(40, 200, 55, 10));
+    for ( auto it = vc.begin(3); it!= vc.end(3); ++it )
+      viewer << it->first;
+
+    viewer << Viewer3D<>::updateDisplay;
+    app.exec();
+  }
+}
