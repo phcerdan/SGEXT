4c004b5ea3f8c8f893281de933818d573c6f4eeb
phcerdan <pablo.hernandez.cerdan@outlook.com>
2016-04-07 23:36:37 +1200

    Add load tables.

--- src/thin.cpp
+++ src/thin.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <unordered_map>
 #include <DGtal/base/Common.h>
 #include <DGtal/helpers/StdDefs.h>
 #include <DGtal/io/readers/ITKReader.h>
@@ -10,10 +11,11 @@
 #include <DGtal/io/boards/Board2D.h>
 #include <DGtal/topology/CubicalComplex.h>
 #include <DGtal/topology/CubicalComplexFunctions.h>
-#include <unordered_map>
 
 #include <DGtal/topology/VoxelComplex.h>
 #include <DGtal/topology/VoxelComplexFunctions.h>
+#include <DGtal/topology/tables/LookUpTableFunctions.h>
+#include <DGtal/topology/tables/ConfigTables.h>
 // Invert
 #include "itkInvertIntensityImageFilter.h"
 #include <DGtal/io/viewers/Viewer3D.h>
@@ -23,6 +25,12 @@
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/variables_map.hpp>
+// Distance Map
+#include "DGtal/kernel/BasicPointPredicates.h"
+#include "DGtal/images/SimpleThresholdForegroundPredicate.h"
+#include "DGtal/geometry/volumes/distance/ExactPredicateLpSeparableMetric.h"
+#include "DGtal/geometry/volumes/distance/VoronoiMap.h"
+#include "DGtal/geometry/volumes/distance/DistanceTransformation.h"
 using namespace DGtal;
 using namespace std;
 using namespace DGtal::Z3i;
@@ -36,6 +44,7 @@ int main(int argc, char* const argv[]){
     ( "help,h", "display this message." )
     ( "input,i", po::value<string>()->required(), "Input vol file." )
     ( "skel,s",  po::value<string>()->required(), "type of skeletonization" )
+    ( "select,c",  po::value<string>()->required(), "select method for skeletonization" )
     ( "foreground,f",  po::value<string>()->default_value("black"), "foreground color in binary image" )
     ( "thresholdMin,m",  po::value<int>()->default_value(0), "threshold min (excluded) to define binary shape" )
     ( "thresholdMax,M",  po::value<int>()->default_value(255), "threshold max (included) to define binary shape" )
@@ -81,8 +90,16 @@ int main(int argc, char* const argv[]){
   string sk_string = vm["skel"].as<string>();
   if (vm.count("skel") &&
      (!( sk_string == "ulti" || sk_string == "end" ||
-         sk_string == "1is" || sk_string == "is")))
+         sk_string == "1is" || sk_string == "is" ||
+         sk_string == "isthmus" || sk_string == "1isthmus"))
+     )
      throw po::validation_error(po::validation_error::invalid_option_value, "skel");
+  string select_string = vm["select"].as<string>();
+  if (vm.count("select") &&
+     (!( select_string == "random" || select_string == "dmax" ||
+         select_string == "first" ))
+     )
+     throw po::validation_error(po::validation_error::invalid_option_value, "select");
   /*-------------- End of parse -----------------------------*/
 
   using Domain = Domain ;
@@ -138,6 +155,7 @@ int main(int argc, char* const argv[]){
   using Complex =
     DGtal::VoxelComplex<KSpace, Object>;
 
+  auto & sk = sk_string;
   KSpace ks;
   ks.init(image.domain().lowerBound() ,
       image.domain().upperBound() , true);
@@ -147,26 +165,68 @@ int main(int argc, char* const argv[]){
   DigitalTopology topo(adjF, adjB, DGtal::DigitalTopologyProperties::JORDAN_DT);
   Object obj(topo,image_set);
 
+  trace.beginBlock("construct with table");
   Complex vc(ks);
-  vc.construct(obj);
-
+  // vc.construct(obj);
+  vc.construct(obj.pointSet(), lut::tableSimple26_6);
+  trace.endBlock();
+  trace.beginBlock("load isthmus table");
+  boost::dynamic_bitset<> isthmus_table;
+  if (sk == "isthmus") isthmus_table = *lut::loadTable(lut::tableIsthmus);
+  else if (sk == "1isthmus") isthmus_table = *lut::loadTable(lut::tableOneIsthmus);
+  auto pointMap = *lut::mapPointToBitMask<Point>();
+  trace.endBlock();
   using namespace DGtal::functions ;
-
+  // SKEL FUNCTION:
   std::function< bool(const Complex&, const Cell&) > Skel ;
-  auto & sk = sk_string;
   if (sk == "ulti") Skel = skelUltimate<Complex>;
   else if (sk == "end") Skel = skelEnd<Complex>;
-  else if (sk == "1is") Skel = oneIshtmus<Complex>;
-  else if (sk == "is") Skel = skelIshtmus<Complex>;
+  else if (sk == "1is") Skel = oneIsthmus<Complex>;
+  else if (sk == "is") Skel = skelIsthmus<Complex>;
+  else if (sk == "1isthmus")
+      Skel = [&isthmus_table, &pointMap](const Complex & fc,
+               const Complex::Cell & c){
+        return skelWithTable(isthmus_table, pointMap, fc, c);
+      };
+  else if (sk == "isthmus")
+      Skel = [&isthmus_table, &pointMap](const Complex & fc,
+               const Complex::Cell & c){
+        return skelWithTable(isthmus_table, pointMap, fc, c);
+      };
   else throw std::runtime_error("Invalid skel string");
   auto start = std::chrono::system_clock::now();
+
+  // SELECT FUNCTION
+  /* TODO fix this(new exe?) DT has no default, no copy constructor so we have
+   * to calculate for every image....
+   */
+
+  trace.beginBlock("Create Distance Map");
+  using Predicate = Z3i::DigitalSet;
+  using L3Metric = ExactPredicateLpSeparableMetric<Z3i::Space, 3>;
+  using DT       = DistanceTransformation<Z3i::Space, Predicate, L3Metric>;
+  L3Metric l3;
+  DT dt(obj.domain(),obj.pointSet(), l3);
+  trace.endBlock();
+
+  std::function< typename Complex::Cell(const Complex::Clique&) > Select ;
+  auto & sel = select_string;
+  if (sel == "random") Select = selectRandom<Complex>;
+  else if (sel == "first") Select = selectFirst<Complex>;
+  else if (sel == "dmax"){
+    Select =
+      [&dt](const Complex::Clique & clique){
+        return selectMaxValue<DT, Complex>(dt,clique);
+      };
+  } else throw std::runtime_error("Invalid skel string");
+
   Complex vc_new(ks);
   if (persistence == 0)
     vc_new = asymetricThinningScheme< Complex >(
-        vc, selectRandom<Complex>,  Skel, verbose);
+        vc, Select,  Skel, verbose);
   else
     vc_new = persistenceAsymetricThinningScheme< Complex >(
-        vc, selectRandom<Complex>,  Skel, persistence, verbose);
+        vc, Select,  Skel, persistence, verbose);
 
   auto end = std::chrono::system_clock::now();
   auto elapsed = std::chrono::duration_cast<std::chrono::seconds> (end - start) ;
