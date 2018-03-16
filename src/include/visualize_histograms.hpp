f80b623d6a64f4c69678b36dd14cc1263413371d
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-03-16 04:24:21 +0100

    Add visualize_histograms with a vector of histograms

---
+++ src/include/visualize_histograms.hpp
@@ -0,0 +1,18 @@
+#ifndef VISUALIZE_HISTOGRAMS_HPP
+#define VISUALIZE_HISTOGRAMS_HPP
+
+#include "histo.hpp"
+
+namespace SG {
+
+/**
+ * Uses Qt and vtk to visualize a vector of histograms.
+ *
+ * @sa histo::visualize_histo
+ *
+ * @param input_histograms
+ */
+void visualize_histograms(const std::vector<histo::Histo<double>> & input_histograms);
+
+}
+#endif
--- src/test/test_visualize_histograms.cpp
+++ src/test/test_visualize_histograms.cpp
@@ -2,14 +2,40 @@
  * See LICENSE on top of this repository. */
 
 #include "catch_header.h"
-#include "visualize_histo.hpp"
+#include "visualize_histo.hpp" // External, from histo.
+#include "visualize_histograms.hpp" // Visualize using Qt and histo.
 
-TEST_CASE("VisualizeHisto", "[withJustData]")
+// Unit testing is done in the external project, this just checks that works here.
+TEST_CASE("ExternalProject Histo works", "[external][visualize][histo]")
 {
     std::vector<double> data{0.0, 1.0, 1.0,1.0, 2.0, 3.0, 5.0, 5.0, 8.0, 8.0,  12.0};
     histo::Histo<double> h(data, histo::GenerateBreaksFromRangeAndBins<double>(0.0,15.0, 5));
     h.PrintBreaksAndCounts(std::cout);
     h.name = "withJustData";
-    visualize_histo(h);
-    visualize_histo(h, vtkChart::BAR);
+    histo::visualize_histo(h);
+    histo::visualize_histo(h, vtkChart::BAR);
+}
+
+TEST_CASE("visualize_histograms", "[visualize][histo]")
+{
+    std::vector<double> data{0.0, 1.0, 1.0,1.0, 2.0, 3.0, 5.0, 5.0, 8.0, 8.0,  12.0};
+    std::vector<histo::Histo<double>> histograms;
+    histograms.emplace_back(
+            histo::Histo<double>(data, histo::GenerateBreaksFromRangeAndBins<double>(0.0,15.0, 5))
+            );
+    histograms.back().name = "h1";
+    histograms.emplace_back(
+            histo::Histo<double>(data, histo::GenerateBreaksFromRangeAndBins<double>(0.0,15.0, 5))
+            );
+    histograms.back().name = "h2";
+    histograms.emplace_back(
+            histo::Histo<double>(data, histo::GenerateBreaksFromRangeAndBins<double>(0.0,15.0, 5))
+            );
+    histograms.back().name = "h3";
+    histograms.emplace_back(
+            histo::Histo<double>(data, histo::GenerateBreaksFromRangeAndBins<double>(0.0,15.0, 5))
+            );
+    histograms.back().name = "h4";
+
+    SG::visualize_histograms(histograms);
 }
