10d284a92a1583f585422a0fd1e5bc7fb9ec1281
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2018-03-29 04:16:24 +0200

    Add license to all files

--- src/include/trim_graph.hpp
+++ src/include/trim_graph.hpp
@@ -1,3 +1,23 @@
+/* ********************************************************************
+ * Copyright (C) 2020 Pablo Hernandez-Cerdan.
+ *
+ * This file is part of SGEXT: http://github.com/phcerdan/sgext.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * *******************************************************************/
+
 #ifndef TRIM_GRAPH_HPP
 #define TRIM_GRAPH_HPP
 #include "spatial_graph.hpp"
@@ -8,7 +28,7 @@ namespace SG {
    * degree 0 (removed or isolated vertices)
    * degree 1 (end points)
    * degree 2 : (self-loops)
-   *    if coming from @reduce_spatial_graph_via_dfs, degree 2 
+   *    if coming from @reduce_spatial_graph_via_dfs, degree 2
    *    mark the middle of a self-loop.
    *    But extra checking of being a self-loop would be safer.
    *
