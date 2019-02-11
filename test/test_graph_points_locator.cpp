00c1df6f72a1f9a92d8ffa8a1d6f4430430041b6
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-02-10 22:14:53 -0500

    Add graph locator functions (radius and NClosest)

--- test/test_graph_points_locator.cpp
+++ test/test_graph_points_locator.cpp
@@ -9,32 +9,32 @@
 
 struct GraphPointLocatorFixture : public ::testing::Test {
     using GraphType = SG::GraphType;
-    GraphType g;
+    GraphType g0;
     GraphType g1;
 
     void SetUp() override {
         using boost::add_edge;
         using boost::add_vertex;
-        this->g = GraphType(3);
+        this->g0 = GraphType(3);
 
         // Add edge with an associated SpatialEdge at construction.
         SG::PointType p0{{0, 0, 0}};
         SG::PointType e1{{1, 0, 0}};
         SG::PointType e2{{2, 0, 0}};
-        g[0].pos = p0;
-        g[1].pos = e1;
-        g[2].pos = e2;
+        g0[0].pos = p0;
+        g0[1].pos = e1;
+        g0[2].pos = e2;
 
         SG::PointType e05{{0.5, 0, 0}};
         SG::PointType e15{{1.5, 0, 0}};
         SG::SpatialEdge se05;
         se05.edge_points.insert(std::end(se05.edge_points), {e05});
-        add_edge(0, 1, se05, g);
+        add_edge(0, 1, se05, g0);
         SG::SpatialEdge se15;
         se15.edge_points.insert(std::end(se15.edge_points), {e15});
-        add_edge(1, 2, se15, g);
+        add_edge(1, 2, se15, g0);
         // Add an extra node and edge to g1
-        this->g1 = g;
+        this->g1 = g0;
         SG::PointType e3{{3, 0, 0}};
         SG::SpatialNode sn3;
         sn3.pos = e3;
@@ -48,14 +48,29 @@ struct GraphPointLocatorFixture : public ::testing::Test {
 
 TEST_F(GraphPointLocatorFixture, just_works)
 {
-    auto point_map_pair = SG::get_vtk_points_from_graph(g);
+    auto point_map_pair = SG::get_vtk_points_from_graph(g0);
     auto kdtree = SG::build_kdtree_locator(point_map_pair.first);
     EXPECT_EQ(kdtree->GetDataSet()->GetNumberOfPoints(), point_map_pair.first->GetNumberOfPoints());
 }
 
 TEST_F(GraphPointLocatorFixture, is_able_to_match_equal_graphs)
 {
-    const auto & g0 =g;
+    std::vector<std::reference_wrapper<const GraphType>> graphs;
+    graphs.reserve(2);
+    graphs.push_back(std::cref(g0));
+    graphs.push_back(std::cref(g0));
+    auto merger_map_pair = SG::get_vtk_points_from_graphs(graphs);
+    auto kdtree = SG::build_kdtree_locator(merger_map_pair.first->GetPoints());
+    EXPECT_EQ(kdtree->GetDataSet()->GetNumberOfPoints(),
+            merger_map_pair.first->GetPoints()->GetNumberOfPoints());
+    SG::PointType testPoint = {{2,0,0}}; // last vertex
+    vtkIdType idClosest = kdtree->FindClosestPoint(testPoint.data());
+    vtkIdType expectedId = 2;
+    EXPECT_EQ(idClosest, expectedId);
+}
+
+TEST_F(GraphPointLocatorFixture, is_able_to_match_almost_equal_graphs)
+{
     std::vector<std::reference_wrapper<const GraphType>> graphs;
     graphs.reserve(2);
     graphs.push_back(std::cref(g0));
@@ -81,16 +96,95 @@ TEST_F(GraphPointLocatorFixture, is_able_to_match_equal_graphs)
     const auto & graph_descriptor_map = merger_map_pair.second;
     const auto & graph_descs = graph_descriptor_map.at(idClosest);
     EXPECT_EQ(graph_descs.size(), 2);
-    SG::print_graph_descriptor(graph_descs[0], "graph0");
-    SG::print_graph_descriptor(graph_descs[1], "graph1");
+    SG::print_graph_descriptor(graph_descs[0], "graph0, id " + std::to_string(idClosest));
+    SG::print_graph_descriptor(graph_descs[1], "graph1, id " + std::to_string(idClosest));
     EXPECT_FALSE(graph_descs[0].exist);
     EXPECT_TRUE(graph_descs[1].exist);
 
-    for(const auto & key_value_pair : graph_descriptor_map){
-        const auto & id = key_value_pair.first;
-        const auto & gdescs = key_value_pair.second;
-        for(const auto & gdesc : gdescs){
-            SG::print_graph_descriptor(gdesc, std::to_string(id));
-        }
-    }
+    // for(const auto & key_value_pair : graph_descriptor_map){
+    //     const auto & id = key_value_pair.first;
+    //     const auto & gdescs = key_value_pair.second;
+    //     for(const auto & gdesc : gdescs){
+    //         SG::print_graph_descriptor(gdesc, std::to_string(id));
+    //     }
+    // }
+}
+
+TEST_F(GraphPointLocatorFixture, graph_closest_n_points_locator) {
+
+    std::vector<std::reference_wrapper<const GraphType>> graphs;
+    graphs.reserve(2);
+    graphs.push_back(std::cref(g0));
+    graphs.push_back(std::cref(g1));
+    auto merger_map_pair = SG::get_vtk_points_from_graphs(graphs);
+    auto kdtree = SG::build_kdtree_locator(merger_map_pair.first->GetPoints());
+    EXPECT_EQ(kdtree->GetDataSet()->GetNumberOfPoints(), 7);
+
+    SG::PointType testPoint = {{3,0,0}};
+    auto out_gdescs = SG::graph_closest_n_points_locator(testPoint, kdtree, merger_map_pair.second);
+    const auto & gdesc0 = out_gdescs[0];
+    SG::print_graph_descriptor(gdesc0);
+    EXPECT_TRUE(gdesc0.exist);
+    EXPECT_TRUE(gdesc0.is_vertex);
+    EXPECT_FALSE(gdesc0.is_edge);
+    EXPECT_EQ(gdesc0.vertex_d, 2);
+    const auto & gdesc1 = out_gdescs[1];
+    SG::print_graph_descriptor(gdesc1);
+    EXPECT_TRUE(gdesc1.exist);
+    EXPECT_TRUE(gdesc1.is_vertex);
+    EXPECT_FALSE(gdesc1.is_edge);
+    EXPECT_EQ(gdesc1.vertex_d, 3);
+}
+
+TEST_F(GraphPointLocatorFixture, graph_closest_points_by_radius_locator) {
+
+    std::vector<std::reference_wrapper<const GraphType>> graphs;
+    graphs.reserve(2);
+    graphs.push_back(std::cref(g0));
+    graphs.push_back(std::cref(g1));
+    auto merger_map_pair = SG::get_vtk_points_from_graphs(graphs);
+    auto kdtree = SG::build_kdtree_locator(merger_map_pair.first->GetPoints());
+    EXPECT_EQ(kdtree->GetDataSet()->GetNumberOfPoints(), 7);
+
+    SG::PointType testPoint = {{3.0,0,0}};
+    double radius = 10.0;
+    auto out_gdescs = SG::graph_closest_points_by_radius_locator(testPoint, kdtree, merger_map_pair.second, radius);
+    const auto & gdesc0 = out_gdescs[0];
+    SG::print_graph_descriptor(gdesc0);
+    EXPECT_TRUE(gdesc0.exist);
+    EXPECT_TRUE(gdesc0.is_vertex);
+    EXPECT_FALSE(gdesc0.is_edge);
+    EXPECT_EQ(gdesc0.vertex_d, 2);
+    const auto & gdesc1 = out_gdescs[1];
+    SG::print_graph_descriptor(gdesc1);
+    EXPECT_TRUE(gdesc1.exist);
+    EXPECT_TRUE(gdesc1.is_vertex);
+    EXPECT_FALSE(gdesc1.is_edge);
+    EXPECT_EQ(gdesc1.vertex_d, 3);
+}
+
+TEST_F(GraphPointLocatorFixture, graph_closest_points_by_radius_locator_small_radius) {
+    std::vector<std::reference_wrapper<const GraphType>> graphs;
+    graphs.reserve(2);
+    graphs.push_back(std::cref(g0));
+    graphs.push_back(std::cref(g1));
+    auto merger_map_pair = SG::get_vtk_points_from_graphs(graphs);
+    auto kdtree = SG::build_kdtree_locator(merger_map_pair.first->GetPoints());
+    EXPECT_EQ(kdtree->GetDataSet()->GetNumberOfPoints(), 7);
+
+    SG::PointType testPoint = {{3.0,0,0}};
+    double radius = 2.0;
+    auto out_gdescs = SG::graph_closest_points_by_radius_locator(testPoint, kdtree, merger_map_pair.second, radius);
+    const auto & gdesc0 = out_gdescs[0];
+    SG::print_graph_descriptor(gdesc0);
+    EXPECT_TRUE(gdesc0.exist);
+    EXPECT_TRUE(gdesc0.is_vertex);
+    EXPECT_FALSE(gdesc0.is_edge);
+    EXPECT_EQ(gdesc0.vertex_d, 2);
+    const auto & gdesc1 = out_gdescs[1];
+    SG::print_graph_descriptor(gdesc1);
+    EXPECT_TRUE(gdesc1.exist);
+    EXPECT_TRUE(gdesc1.is_vertex);
+    EXPECT_FALSE(gdesc1.is_edge);
+    EXPECT_EQ(gdesc1.vertex_d, 3);
 }
