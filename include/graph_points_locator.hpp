11c985ed529f06064584dfc856253ed5cd9b2b01
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-02-10 22:14:53 -0500

    Add graph locator functions (radius and NClosest)

--- include/graph_points_locator.hpp
+++ include/graph_points_locator.hpp
@@ -29,7 +29,50 @@
 
 namespace SG {
 
-  vtkSmartPointer<vtkKdTreePointLocator> build_kdtree_locator(vtkPoints * inputPoints);
+  namespace detail {
+  /**
+   * Utility function re-used in other function to
+   *
+   * @param in_out_gdescs
+   * @param closeIdList
+   * @param idMap
+   */
+  void fill_graph_descriptors(std::vector<graph_descriptor> & in_out_gdescs,
+      vtkIdList * closeIdList,
+      const std::unordered_map<vtkIdType, std::vector<graph_descriptor>> & idMap);
+  } //ns detail
 
+  /**
+   * Builds a kdtree from input points
+   *
+   * @param inputPoints vtk points extracted from a spatial graph
+   *
+   * @return the kdtree point locator
+   */
+  vtkSmartPointer<vtkKdTreePointLocator> build_kdtree_locator(vtkPoints * inputPoints);
+  /**
+   * False if any gdesc.exist == false;
+   *
+   * @param gdescs
+   *
+   * @return false if any gdesc.exist == false
+   */
+  bool all_graph_descriptors_exist(const std::vector<graph_descriptor> & gdescs);
+
+  /**
+   * Use the kdtree point locator and the idMap from a set of graphs to query a point in space.
+   * It returns a vector of graph_descriptors.
+   *
+   * @param queryPoint
+   * @param kdtree
+   * @param
+   */
+  std::vector<graph_descriptor> graph_closest_n_points_locator(const PointType &queryPoint, vtkKdTreePointLocator * kdtree,
+      const std::unordered_map<vtkIdType, std::vector<graph_descriptor>> & idMap,
+      const int closest_n_points = 5 );
+
+  std::vector<graph_descriptor> graph_closest_points_by_radius_locator(const PointType &queryPoint, vtkKdTreePointLocator * kdtree,
+      const std::unordered_map<vtkIdType, std::vector<graph_descriptor>> & idMap,
+      double radius);
 } // ns SG
 #endif
