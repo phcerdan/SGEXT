482c60effbc73cec63c23ce86643d03aa46bc72c
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-03-11 17:38:52 -0400

    Fix program options bug in analyze_graph.
    Improve pipeline

--- utilities/pipeline.sh
+++ utilities/pipeline.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "DATA_FOLDER=${DATA_FOLDER}"
+echo "INPUT_LABEL=${INPUT_LABEL}"
+echo "BINARY_FOLDER=${BINARY_FOLDER}"
 # DATA_FOLDER=~/Data
 # INPUT_LABEL=file_name_without_extension
 # BINARY_FOLDER=~/repository_local/object_to_spatial_graph/build-debug/cpp-scripts
@@ -7,11 +10,16 @@
 # Compute Distance Map
 # Thin with DMAP, 1isthmus and 0 persistence
 # Create a serialized graph
+echo "STARTING fill_holes_binary" && \
 $BINARY_FOLDER/fill_holes_binary -i $DATA_FOLDER/${INPUT_LABEL}.nrrd -o $DATA_FOLDER/results -v -z && \
+echo "STARTING create_distance_map" && \
 $BINARY_FOLDER/create_distance_map -i $DATA_FOLDER/results/${INPUT_LABEL}_FILLED.nrrd -o $DATA_FOLDER/results -v && \
+echo "STARTING thin" && \
 $BINARY_FOLDER/thin -i $DATA_FOLDER/results/${INPUT_LABEL}_FILLED.nrrd -o $DATA_FOLDER/results \
     -s 1isthmus -c dmax -d ${DATA_FOLDER}/results/${INPUT_LABEL}_FILLED_DMAP.nrrd -v -z && \
+echo "STARTING analyze_graph" && \
 $BINARY_FOLDER/analyze_graph -i $DATA_FOLDER/results/${INPUT_LABEL}_FILLED_SKEL.nrrd -o $DATA_FOLDER/results -u $DATA_FOLDER/results \
-   --reduceGraph --removeExtraEdges -m -v -z
+   --reduceGraph --removeExtraEdges --mergeThreeConnectedNodes -v -z && \
+echo "PIPELINE FINISHED SUCCESFULLY"
 
 
