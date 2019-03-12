48bae6f83da3eb1b796087d0e054f0dcfc1d51fe
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-03-12 05:52:29 -0400

    Tweak script to visualize merged graphs

--- utilities/visualize_low_high.sh
+++ utilities/visualize_low_high.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 echo "DATA_FOLDER=${DATA_FOLDER}"
+# INPUT_LABEL should be set to the high graph!
 echo "INPUT_LABEL=${INPUT_LABEL}"
 echo "BINARY_FOLDER=${BINARY_FOLDER}"
+export LOW_GRAPH_LABEL=$(echo "$INPUT_LABEL" | sed 's/high/low/g'); echo "LOW_GRAPH_LABEL: $LOW_GRAPH_LABEL"
+export HIGH_GRAPH_LABEL=$(echo "$INPUT_LABEL" | sed 's/low/high/g'); echo "HIGH_GRAPH_LABEL: $HIGH_GRAPH_LABEL"
 
-echo "VISUALIZE low" && \
-$BINARY_FOLDER/visualize_graph_and_image -s $DATA_FOLDER/results/${INPUT_LABEL}_FILLED_SKEL.nrrd \
--g $DATA_FOLDER/results/${INPUT_LABEL}_FILLED_SKEL_REDUCED.txt -u & \
 echo "VISUALIZE high" && \
-INPUT_LABEL=$(echo "$INPUT_LABEL" | sed 's/low/high/g'); echo $INPUT_LABEL && \
-$BINARY_FOLDER/visualize_graph_and_image -s $DATA_FOLDER/results/${INPUT_LABEL}_FILLED_SKEL.nrrd \
--g $DATA_FOLDER/results/${INPUT_LABEL}_FILLED_SKEL_REDUCED.txt -u &
+$BINARY_FOLDER/visualize_graph_and_image -s $DATA_FOLDER/results/${HIGH_GRAPH_LABEL}_FILLED_SKEL.nrrd \
+-g $DATA_FOLDER/results/${HIGH_GRAPH_LABEL}_FILLED_SKEL_REDUCED.txt -u & \
+echo "VISUALIZE low" && \
+$BINARY_FOLDER/visualize_graph_and_image -s $DATA_FOLDER/results/${LOW_GRAPH_LABEL}_FILLED_SKEL.nrrd \
+-g $DATA_FOLDER/results/${LOW_GRAPH_LABEL}_FILLED_SKEL_REDUCED.txt -u &
+merged_graph=$DATA_FOLDER/results/${HIGH_GRAPH_LABEL}_FILLED_SKEL_REDUCED_MERGED.txt
+if [ -f ${merged_graph} ]; then
+   echo "VISUALIZE merged" && \
+      $BINARY_FOLDER/visualize_graph_and_image -s $DATA_FOLDER/results/${LOW_GRAPH_LABEL}_FILLED_SKEL.nrrd \
+      -g ${merged_graph} -u
+fi
 
