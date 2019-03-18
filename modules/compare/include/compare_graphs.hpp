1f63524d8ff56b1d37ba28a8b6739e7306055e64
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-03-18 01:09:13 -0400

    BUG: Add header guard to compare_graph

--- modules/compare/include/compare_graphs.hpp
+++ modules/compare/include/compare_graphs.hpp
@@ -2,6 +2,8 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef COMPARE_GRAPHS_HPP
+#define COMPARE_GRAPHS_HPP
 
 #include "spatial_graph.hpp"
 #include "filter_spatial_graph.hpp"
@@ -18,3 +20,5 @@ GraphType compare_low_and_high_info_graphs(const GraphType& g0,
                                            const GraphType& g1,
                                            const double radius = 2.0);
 }  // namespace SG
+
+#endif
