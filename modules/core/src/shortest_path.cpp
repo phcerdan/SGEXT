63e03f6c30e5b7418480917d9236c5cb9dd79182
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-03-19 18:30:50 -0400

    ENH: Add compute_shortest_path between two vertices using dijkstra
    It uses the countour length of the edge as weight.

---
+++ modules/core/src/shortest_path.cpp
@@ -0,0 +1,87 @@
+/* Copyright (C) 2019 Pablo Hernandez-Cerdan
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "shortest_path.hpp"
+#include "edge_points_utilities.hpp"
+#include <boost/property_map/function_property_map.hpp>
+
+namespace SG {
+
+std::vector<GraphType::vertex_descriptor> compute_shortest_path(
+    GraphType::vertex_descriptor start_vertex,
+    GraphType::vertex_descriptor end_vertex, const GraphType &input_g,
+    bool verbose) {
+  using vertex_descriptor = GraphType::vertex_descriptor;
+  using edge_descriptor = GraphType::edge_descriptor;
+  using edge_iterator = GraphType::edge_iterator;
+  size_t visited = 0;
+  std::vector<boost::default_color_type> colors(boost::num_vertices(input_g),
+                                                boost::default_color_type{});
+  std::vector<vertex_descriptor> _pred(boost::num_vertices(input_g),
+                                       input_g.null_vertex());
+  std::vector<size_t> _dist(boost::num_vertices(input_g), -1ull);
+
+  auto colormap = colors.data();
+  auto predmap = _pred.data();
+  auto distmap = _dist.data();
+
+  // From sehe:
+  // https://stackoverflow.com/questions/29348724/dijkstra-graph-with-a-table-of-weights-on-each-edge
+  auto weight_f = [&input_g](edge_descriptor ed) {
+    // auto source = boost::source(ed, input_g);
+    // auto target = boost::target(ed, input_g);
+    // return ArrayUtilities::distance(input_g[source].pos,
+    // input_g[target].pos);
+    return SG::contour_length(ed, input_g);
+  };
+
+  auto weightmap =
+      boost::make_function_property_map<edge_descriptor, double>(weight_f);
+  shortest_path_visitor vis(end_vertex, visited);
+
+  try {
+    if(verbose)
+      std::cout << "Searching from #" << start_vertex << " to #" << end_vertex << "...\n";
+    boost::dijkstra_shortest_paths(input_g, start_vertex,
+                                   boost::visitor(vis)
+                                       .color_map(colormap)
+                                       .distance_map(distmap)
+                                       .predecessor_map(predmap)
+                                       .weight_map(weightmap));
+    // .weight_map(boost::make_constant_property<edge_descriptor>(1ul)));
+  } catch(shortest_path_visitor::done &) {
+    if(verbose)
+      std::cout << "Completed. Percentage visited: "
+        << (100.0 * visited / boost::num_vertices(input_g)) << "%\n";
+  }
+
+  size_t distance = distmap[end_vertex];
+  if(verbose)
+    std::cout << "Distance from #" << start_vertex << " to #" << end_vertex
+      << ": " << distance << "\n";
+
+  std::vector<vertex_descriptor> path_out;
+  if(distance != size_t(-1)) {
+    std::deque<vertex_descriptor> path;
+    for(vertex_descriptor current = end_vertex;
+        current != input_g.null_vertex() && predmap[current] != current &&
+        current != start_vertex;) {
+      path.push_front(predmap[current]);
+      current = predmap[current];
+    }
+
+    if(verbose) {
+      std::cout << "Path from #" << start_vertex << " to #" << end_vertex << ": ";
+      std::copy(path.begin(), path.end(),
+          std::ostream_iterator<vertex_descriptor>(std::cout, ", "));
+      std::cout << end_vertex << "\n";
+    }
+
+    std::copy(path.begin(), path.end(), std::back_inserter(path_out));
+    path_out.push_back(end_vertex);
+  }
+  return path_out;
+}
+}  // end namespace SG
---
+++ modules/core/test/test_shortest_path.cpp
@@ -0,0 +1,58 @@
+/* Copyright (C) 2019 Pablo Hernandez-Cerdan
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "array_utilities.hpp"
+#include "gmock/gmock.h"
+#include "shortest_path.hpp"
+#include "spatial_graph.hpp"
+#include "spatial_graph_utilities.hpp"
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+
+struct ShortestPathFixture : public ::testing::Test {
+  using GraphType = SG::GraphAL;
+  using vertex_descriptor = GraphType::vertex_descriptor;
+  GraphType g;
+
+  void SetUp() override {
+    using boost::add_edge;
+    this->g = GraphType(3);
+
+    // Add edge with an associated SpatialEdge at construction.
+    SG::PointType p0{{0, 0, 0}};
+    SG::PointType e1{{1, 0, 0}};
+    SG::PointType e2{{2, 0, 0}};
+    SG::PointType e05{{0.5, 0, 0}};
+    SG::PointType e15{{1.5, 0, 0}};
+
+    g[0].pos = p0;
+    g[1].pos = e1;
+    g[2].pos = e2;
+
+    SG::SpatialEdge se05;
+    se05.edge_points.insert(std::end(se05.edge_points), {e05});
+    add_edge(0, 1, se05, g);
+    SG::SpatialEdge se15;
+    se15.edge_points.insert(std::end(se15.edge_points), {e15});
+    add_edge(1, 2, se15, g);
+  }
+};
+
+TEST_F(ShortestPathFixture, works) {
+  // Setup data
+  vertex_descriptor start_vertex = 0;
+  vertex_descriptor end_vertex = 2;
+  bool verbose = true;
+  auto shortest_path =
+      SG::compute_shortest_path(start_vertex, end_vertex, g, verbose);
+  std::copy(shortest_path.begin(), shortest_path.end(),
+      std::ostream_iterator<vertex_descriptor>(std::cout, ", "));
+  std::cout << std::endl;
+  // Read data
+  EXPECT_EQ(shortest_path.size(), 3);
+  std::vector<vertex_descriptor> expected_shortest_path = {0,1,2};
+  EXPECT_EQ(shortest_path, expected_shortest_path);
+}
