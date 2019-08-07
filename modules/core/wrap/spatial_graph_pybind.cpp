478a28af64dca88bca44c5fff7a00530270b4862
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-08-07 18:57:29 +0200

    Working pybind11 wrapping for SGCore
    Add wrappings for:
    - ArrayUtilities::Array3D
    - spatial_node
    - spatial_edge
    - spatial_graph:
        - edge_descriptor, vertex_descriptor
        - boost::add_edge(u, v, SpatialEdge(), GraphType)
    
    TODO: wrap more

---
+++ modules/core/wrap/spatial_graph_pybind.cpp
@@ -0,0 +1,53 @@
+/* Copyright (C) 2019 Pablo Hernandez-Cerdan
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "sgcore_common.hpp"
+#include "spatial_graph.hpp"
+#include "spatial_graph_utilities.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
+namespace py = pybind11;
+using namespace SG;
+
+void init_spatial_graph(py::module &m) {
+    auto mgraph = m.def_submodule("graph");
+    py::class_<GraphType::vertex_descriptor>(m, "vertex_descriptor")
+            .def(py::init());
+            // TODO probably missing something wrappable in vertex_descriptor
+    py::class_<GraphType::edge_descriptor>(m, "edge_descriptor")
+            .def(py::init())
+            // No access to pointer m_property from python for now
+            // The pointer in the constructor is confusing,
+            // use a lambda and ignore SpatialEdge for descriptors...
+            // probably not used anyway
+            // .def(py::init<size_t, size_t, SpatialEdge*>())
+            .def_readwrite("source", &GraphType::edge_descriptor::m_source)
+            .def_readwrite("target", &GraphType::edge_descriptor::m_target)
+            .def("__repr__",
+                 [](const GraphType::edge_descriptor &ed) {
+                     return "<sgcore.edge_descriptor source: " + std::to_string(ed.m_source) + "; target: " + std::to_string(ed.m_target) + " >";
+                 });
+    py::class_<GraphType>(m, "spatial_graph")
+            .def(py::init())
+            .def(py::init<size_t>())
+            .def("__repr__",
+                 [](const GraphType &graph) {
+                     std::stringstream os;
+                     print_degrees(graph, os);
+                     print_spatial_edges(graph, os);
+                     return "<sgcore.spatial_graph: " + os.str() + ">";
+                 });
+            // return type: Not wrapped
+            // .def_readwrite("vertices", &GraphType::m_vertices)
+            // .def_readwrite("edges", &GraphType::m_edges);
+
+    mgraph.def("num_edge_points", &num_edge_points<GraphType>);
+    mgraph.def("add_edge",
+          [](size_t u, size_t v, const SpatialEdge &se, GraphType &graph) {
+              return boost::add_edge(u, v, se, graph);
+          });
+}
