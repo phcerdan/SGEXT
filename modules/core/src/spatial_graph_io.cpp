b4ea9646c8bd759d11887890697abb24e197ab1a
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-08-12 22:14:26 +0200

    Rename functions dealing with dynamic_properties_sg
    Adding get_ to write and read versions

--- modules/core/src/spatial_graph_io.cpp
+++ modules/core/src/spatial_graph_io.cpp
@@ -27,14 +27,14 @@
 
 namespace SG {
 
-boost::dynamic_properties write_dynamic_properties_sg(GraphType &graph) {
+boost::dynamic_properties get_write_dynamic_properties_sg(GraphType &graph) {
     boost::dynamic_properties dp;
     dp.property("node_id", boost::get(boost::vertex_index, graph));
     dp.property("spatial_node", boost::get(boost::vertex_bundle, graph));
     dp.property("spatial_edge", boost::get(boost::edge_bundle, graph));
     return dp;
 }
-boost::dynamic_properties read_dynamic_properties_sg(GraphType &graph) {
+boost::dynamic_properties get_read_dynamic_properties_sg(GraphType &graph) {
     boost::dynamic_properties dp;
     dp.property("node_id", boost::get(&SpatialNode::id, graph));
     dp.property("spatial_node", boost::get(boost::vertex_bundle, graph));
@@ -43,21 +43,21 @@ boost::dynamic_properties read_dynamic_properties_sg(GraphType &graph) {
 }
 
 void write_graphviz_sg(std::ostream &os, GraphType &graph) {
-    auto dp = write_dynamic_properties_sg(graph);
+    auto dp = get_write_dynamic_properties_sg(graph);
     boost::write_graphviz_dp(os, graph, dp);
 }
 void write_graphviz_sg(const std::string &output_file, GraphType &graph) {
-    auto dp = write_dynamic_properties_sg(graph);
+    auto dp = get_write_dynamic_properties_sg(graph);
     std::ofstream ofile(output_file);
     boost::write_graphviz_dp(ofile, graph, dp);
 }
 
 void read_graphviz_sg(std::istream &is, GraphType &graph) {
-    auto dp = read_dynamic_properties_sg(graph);
+    auto dp = get_read_dynamic_properties_sg(graph);
     boost::read_graphviz(is, graph, dp);
 }
 void read_graphviz_sg(const std::string &input_file, GraphType &graph) {
-    auto dp = read_dynamic_properties_sg(graph);
+    auto dp = get_read_dynamic_properties_sg(graph);
     std::ifstream ifile(input_file);
     boost::read_graphviz(ifile, graph, dp);
 }
