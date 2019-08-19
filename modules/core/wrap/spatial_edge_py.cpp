86a25279a06ef13ccc1a122290c4f34316d1d1a7
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-08-19 10:57:24 +0200

    Modularize python tests
    Add python tests (with unittest) to cmake
    
    Run with `ctest -R python-test-* -V`

--- modules/core/wrap/spatial_edge_py.cpp
+++ modules/core/wrap/spatial_edge_py.cpp
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "sgcore_common.hpp"
+#include "sgcore_common_py.hpp"
 #include "spatial_edge.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
