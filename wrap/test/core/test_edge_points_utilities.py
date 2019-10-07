bb63c6ef0625a326c8b081f2ee916932464aab62
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-10-07 11:44:24 +0200

    All python test automatically passing in ctest

--- wrap/test/core/test_edge_points_utilities.py
+++ wrap/test/core/test_edge_points_utilities.py
@@ -3,32 +3,32 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import sgcore
+import _sgext.core as core
 import unittest
 
 class TestEdgePointsUtilities(unittest.TestCase):
     def test_edge_points_length(self):
-        se = sgcore.spatial_edge()
-        se.edge_points = [sgcore.array.array3d(0,0,0), sgcore.array.array3d(1,0,0)]
-        contour = sgcore.edge_points_length(se)
+        se = core.spatial_edge()
+        se.edge_points = [core.array.array3d(0,0,0), core.array.array3d(1,0,0)]
+        contour = core.edge_points_length(se)
         self.assertGreaterEqual(contour, 1)
 
     def test_contour_length(self):
-        graph = sgcore.spatial_graph(2)
-        sn = sgcore.spatial_node()
-        sn.pos = sgcore.array.array3d(1.0,1.0,1.0)
+        graph = core.spatial_graph(2)
+        sn = core.spatial_node()
+        sn.pos = core.array.array3d(1.0,1.0,1.0)
         graph.set_vertex(1, sn)
-        [ed, added] = sgcore.graph.add_edge(0,1,sgcore.spatial_edge(), graph)
-        contour = sgcore.contour_length(ed, graph)
+        [ed, added] = core.graph.add_edge(0,1,core.spatial_edge(), graph)
+        contour = core.contour_length(ed, graph)
         self.assertGreaterEqual(contour, 1)
 
     def test_insert_unique_edge_point_with_distance_order(self):
-        point0 = sgcore.array.array3d(0,0,0)
-        point1 = sgcore.array.array3d(1,0,0)
-        point2 = sgcore.array.array3d(2,0,0)
-        new_point = sgcore.array.array3d(3,0,0)
+        point0 = core.array.array3d(0,0,0)
+        point1 = core.array.array3d(1,0,0)
+        point2 = core.array.array3d(2,0,0)
+        new_point = core.array.array3d(3,0,0)
         point_container = [point0, point1, point2]
-        new_point_container = sgcore.insert_unique_edge_point_with_distance_order(
+        new_point_container = core.insert_unique_edge_point_with_distance_order(
             point_container, new_point)
         self.assertEqual(len(point_container), 3)
         self.assertEqual(len(new_point_container), 4)
