4f762d5248a5a0d63e34de14332ce8decbb78867
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-11-28 14:04:17 +0100

    Refactor ParticleGraphGlueData

--- modules/dynamics/src/dynamics_graph_glue.cpp
+++ modules/dynamics/src/dynamics_graph_glue.cpp
@@ -28,7 +28,7 @@ ParticleGraphGlueData particles_from_graph(const GraphType &graph) {
     auto &particle_graph_map = glue_data.particle_graph_map;
     auto &connected_list = glue_data.connected_list;
     particles.reserve(num_vertices);
-    connected_list.reserve(num_vertices);
+    connected_list.collection.reserve(num_vertices);
     // we assign consecutive ids to particle_collection, so it's sorted.
     glue_data.particle_collection.sorted = true;
     size_t particle_id = 0;
@@ -38,14 +38,14 @@ ParticleGraphGlueData particles_from_graph(const GraphType &graph) {
         particles.emplace_back(particle);
         particle_graph_map[particle_id] = *ui;
         const auto degree = boost::out_degree(*ui, graph);
-        ConnectedParticles connected_particles;
+        ParticleNeighbors connected_particles;
         connected_particles.particle_id = particle_id;
         connected_particles.neighbors.reserve(degree);
         for (auto [adj_i, adj_i_end] = boost::adjacent_vertices(*ui, graph);
              adj_i != adj_i_end; ++adj_i) {
             connected_particles.neighbors.emplace_back(*adj_i);
         }
-        connected_list.emplace_back(connected_particles);
+        connected_list.collection.emplace_back(connected_particles);
     }
     return glue_data;
 };
