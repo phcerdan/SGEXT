1b021d7f083e556a40109938febcdb6e89473716
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-04-13 18:19:08 +0200

    Add build-tbb in linux

---
+++ deploy/scripts/build-tbb-linux.sh
@@ -0,0 +1,54 @@
+set -x
+
+# Following Arch package:
+# https://git.archlinux.org/svntogit/packages.git/tree/trunk/PKGBUILD?h=packages/intel-tbb
+tbb_pkgver="2020.2"
+tbb_pkgrel="1"
+tbb_url="https://github.com/intel/tbb/archive/v$tbb_pkgver.tar.gz"
+tbb_tar_file="tbb.tar.gz"
+work_directory="/tmp"
+source_folder="oneTBB-$tbb_pkgver"
+make_j="12"
+install_folder="/tmp/tbb-install"
+if [ "$#" = "1" ]; then
+  install_folder="$1"
+fi
+echo "Installing tbb to $install_folder"
+
+function download_package() {
+pushd $work_directory
+curl -L $tbb_url > $tbb_tar_file
+tar -xf $tbb_tar_file
+popd
+};
+
+function build_package() {
+pushd $work_directory
+cd $source_folder
+make -j $make_j
+popd
+}
+
+function install_package() {
+pushd $work_directory
+cd oneTBB-$tbb_pkgver
+install -d $install_folder/lib
+install -m755 build/linux_*/*.so* $install_folder/lib
+install -d $install_folder/include
+cp -a include/tbb $install_folder/include
+cmake \
+  -DINSTALL_DIR=$install_folder/lib/cmake/TBB \
+  -DSYSTEM_NAME=Linux -DTBB_VERSION_FILE=$install_folder/include/tbb/tbb_stddef.h \
+  -P cmake/tbb_config_installer.cmake
+popd
+}
+
+clean_package() {
+rm $work_directory/$tbb_tar_file
+rm -r $work_directory/$source_folder
+}
+
+download_package
+build_package
+install_package
+clean_package
