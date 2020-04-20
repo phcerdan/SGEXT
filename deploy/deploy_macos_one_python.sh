2b8981503b436ea25ab2466527eb0d1c6ee0ec84
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-04-20 22:40:42 +0200

    Add azure-pipeline for deploy multi-platform

--- deploy/deploy_macos_one_python.sh
+++ deploy/deploy_macos_one_python.sh
@@ -5,19 +5,21 @@
 
 script_dir=$(cd $(dirname $0) || exit 1; pwd)
 
+# Requires DEPENDENCIES_BUILD_DIR
+echo "In deploy script: DEPENDENCIES_BUILD_DIR $DEPENDENCIES_BUILD_DIR"
 echo ""
 echo "$(python --version)"
 
 python -m pip install cmake
 python -m pip install ninja
 python -m pip install delocate
-python -m pip install -r requirements-deploy.txt
+python -m pip install -r $script_dir/requirements-deploy.txt
+
 
 pushd ${script_dir}
 python setup.py bdist_wheel --build-type Release -G Ninja -- \
   -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9 \
   -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
-  -DCMAKE_CXX_STANDARD=14 \
   -DSG_BUILD_TESTING:BOOL=OFF \
   -DSG_MODULE_ANALYZE:BOOL=ON \
   -DSG_MODULE_COMPARE:BOOL=ON \
@@ -26,11 +28,8 @@ python setup.py bdist_wheel --build-type Release -G Ninja -- \
   -DSG_MODULE_DYNAMICS:BOOL=ON \
   -DSG_MODULE_VISUALIZE:BOOL=OFF \
   -DSG_MODULE_VISUALIZE_WITH_QT:BOOL=OFF \
-  -DBOOST_ROOT=${BOOST_BUILD_DIR} \
-  -DITK_DIR:STRING=${ITK_BUILD_DIR} \
-  -DDGtal_DIR:STRING=${DGtal_BUILD_DIR} \
   -DSG_WRAP_PYTHON:BOOL=ON \
-
+  -DDEPENDENCIES_BUILD_DIR:PATH=$DEPENDENCIES_BUILD_DIR \
   || exit 1
   # ${PYBIN}/python setup.py clean
 
