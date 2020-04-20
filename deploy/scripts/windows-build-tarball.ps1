4875fde437555b0ff6b714cd066a5e0e9b583fc7
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-04-20 20:20:18 +0200

    Remove unused deploy scripts

--- deploy/scripts/windows-build-tarball.ps1
+++
@@ -1,7 +0,0 @@
-# This script creates a tarball of the ITK Python package build tree. It is
-# downloaded by the external module build scripts and used to build their
-# Python package on GitHub CI services.
-
-cd C:\P\
-Remove-Item IPP\dist\*
-C:\7-Zip\7z.exe a -r 'C:\P\ITKPythonBuilds-windows.zip' -w 'C:\P\IPP' -mem=AES256
