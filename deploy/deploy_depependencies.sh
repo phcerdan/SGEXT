b546fd897d56f8f32b14e5b70b5b1775bd7b1947
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-04-20 22:24:33 +0200

    Deploy scripts for macOS and windows
    Also create deploy_depependencies multi-platform
    for building the subproject in `dependencies`

---
+++ deploy/deploy_depependencies.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd $(dirname $0) || exit 1; pwd)
+dependencies_src_dir=$script_dir/../dependencies
+
+# REQUIRES existing DEPENDENCIES_BUILD_DIR
+
+if [ ! -d "$DEPENDENCIES_BUILD_DIR" ]; then
+  echo ERROR: DEPENDENCIES_BUILD_DIR does not exist: $DEPENDENCIES_BUILD_DIR
+  exit 1
+fi
+echo DEPENDENCIES_BUILD_DIR: $DEPENDENCIES_BUILD_DIR
+
+export CMAKE_EXECUTABLE=cmake
+export SGEXT_BUILD_TYPE=Release
+export GENERATOR=Ninja
+if [[ $OSTYPE == darwin* ]]; then
+  export CMAKE_OS_VARIABLES="\
+  -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9 \
+  -DCMAKE_OSX_ARCHITECTURES:STRING=x86_64 \
+  -DCMAKE_CXX_STANDARD=14 \
+  "
+fi
+
+# Windows: git bash (mysys) or WSL (cygwin)
+if [[ $OSTYPE == mysys* || $OSTYPE == cygwin ]]; then
+  export CMAKE_EXECUTABLE=cmake.exe
+  export GENERATOR="Visual Studio 16 2019"
+  export CMAKE_OS_VARIABLES="\
+  -DCMAKE_CXX_STANDARD=14 \
+  "
+fi
+
+pushd ${DEPENDENCIES_BUILD_DIR}
+$CMAKE_EXECUTABLE \
+  $dependencies_src_dir \
+  -DCMAKE_BUILD_TYPE=$SGEXT_BUILD_TYPE \
+  -G $GENERATOR \
+  $CMAKE_OS_VARIABLES \
+  -DWITH_TBB:BOOL=OFF \
+  -DWITH_VTK:BOOL=OFF \
+  -DWITH_ITK:BOOL=ON \
+  || exit 1
+$CMAKE_EXECUTABLE --build .
+popd
