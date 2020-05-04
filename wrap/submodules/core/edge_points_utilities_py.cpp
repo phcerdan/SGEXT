b885c3df7110f96c7d5d4356d8763cb52e01e7bb
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-05-04 12:46:29 +0200

    Python: Remove MAKE_OPAQUE of Array3D
    Use standard pybind11 bindings of stl types (`std::array<double, 3>`)
    
    https://pybind11.readthedocs.io/en/stable/advanced/cast/stl.html
    
    Care has to be taken in def_readwrite data where references
    don't work.
    A extra function has been provided to classes with a Array3D as readwrite,
    set_pos(index, value)
    
    note that replacing the whole array is valid, so
    `my_class.pos = new_pos` is ok but:
    `my_class.pos[0] = 34.0` is not (my_class.pos is not modified)
    But we can do:
    `my_class.set_pos(0, 34.0)`
    
    Price to pay for having a nicer interface...

--- wrap/submodules/core/edge_points_utilities_py.cpp
+++ wrap/submodules/core/edge_points_utilities_py.cpp
@@ -4,7 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "pybind11_common.h"
-#include "sgcore_common_py.hpp"
 #include <pybind11/stl.h>
 
 #include "edge_points_utilities.hpp"
