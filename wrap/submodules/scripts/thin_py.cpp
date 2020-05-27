f6b43414d563b5e95fff05534a5d62540a375e9d
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-05-27 17:38:48 +0200

    Add tables_folder argument to thin functions.
    `tables_folder` is a string holding the path to the folder containing
    look-up-tables used in DGtal for thinning.
    - simplicity_table26_6.zlib
    - isthmusicityOne_table26_6.zlib
    - isthmusicity_table26_6.zlib
    
    Before this patch, `thin_function` was using the hardcoded table path
    from the generated DGtal header `<DGtal/topology/tables/NeighborhoodTables.h>`
    The paths in that header are correct when DGtal has been built and/or
    installed in the same system where you execute the thinning. However,
    the paths are obviously not correct when distributing the library to
    other machines.
    
    The `cpp-scripts/thin` accepts a tables_folder argument, but use as
    default the one from DGtal, so nothing to change for users.
    
    In python, the 3 aforementioned look-up tables are distributed along the
    sgext package (using `package_data` from setuptools) and can be found in
    `sgext/tables` folder.
    To facilitate its use, a variable tables_folder is populated in the
    `__init__` to the location of that folder.
    ```python
    from sgext import tables_folder
    import sgext
    tables_folder=sgext.tables_folder
    thin(...
        tables_folder=tables_folder
        ...)
    ```

--- wrap/submodules/scripts/thin_py.cpp
+++ wrap/submodules/scripts/thin_py.cpp
@@ -34,6 +34,12 @@ select_type: str
     - dmax: choose voxel with greatest distance map value.
     Strategy to choose voxels in the asymmetric process.
 
+table_folder: str
+    Location of the DGtal look-up-tables for simplicity and isthmusicity,
+    for example simplicity_table26_6.zlib.
+    These tables are distributed with the sgext package. Use the variable
+    'sgext.tables_folder'.
+
 persistence: int
     if >0, performs a persistence algorithm that prunes
     branches that are not persistant (less important).
@@ -56,6 +62,7 @@ visualize: bool
             py::arg("input"),
             py::arg("skel_type"),
             py::arg("select_type"),
+            py::arg("tables_folder"),
             py::arg("persistence") = 0,
             py::arg("input_distance_map_image") = nullptr,
             py::arg("profile") = false,
@@ -90,6 +97,12 @@ select_type: str
 out_folder: str
     output folder to store the results.
 
+table_folder: str
+    Location of the DGtal look-up-tables for simplicity and isthmusicity,
+    for example simplicity_table26_6.zlib.
+    These tables are distributed with the sgext package. Use the variable
+    'sgext.tables_folder'.
+
 persistence: int
     if >0, performs a persistence algorithm that prunes
     branches that are not persistant (less important).
@@ -121,6 +134,7 @@ visualize: bool
             py::arg("skel_type"),
             py::arg("select_type"),
             py::arg("out_folder"),
+            py::arg("tables_folder"),
             py::arg("persistence") = 0,
             py::arg("input_distance_map_file") = "",
             py::arg("foreground") = "white",
--- wrap/test/itk/test_from_to_itk.py
+++ wrap/test/itk/test_from_to_itk.py
@@ -4,6 +4,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from sgext import scripts
+from sgext import tables_folder
 import unittest
 import os, tempfile
 import numpy as np
@@ -23,6 +24,7 @@ class TestFromToITK(unittest.TestCase):
                           out_folder=cls.test_dir,
                           foreground="black",
                           skel_type="end", select_type="first",
+                          tables_folder=tables_folder,
                           persistence=2,
                           visualize=False,
                           verbose=False)
