f3188b0a190bf79412abf7eb2856abf13c2e4683
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-05-28 15:11:54 +0200

    Add check_edge_points_are_contiguous to edge_points_utilities
    Function to check if all edge_points of an edge are contiguous.
    Only works when working in index space (spacing = [1,1,1]).
    
    Wrapped in sgext.core.check_edge_points_are_contiguous(edge_points)
    
    Provide cpp and python tests.

--- wrap/submodules/core/edge_points_utilities_py.cpp
+++ wrap/submodules/core/edge_points_utilities_py.cpp
@@ -12,14 +12,47 @@ namespace py = pybind11;
 using namespace SG;
 
 void init_edge_points_utilities(py::module &m) {
-    m.def("edge_points_length", &edge_points_length);
-    m.def("contour_length", &contour_length);
-    m.def("insert_unique_edge_point_with_distance_order",
-          [](SpatialEdge::PointContainer &edge_points,
-             const SpatialEdge::PointType &new_point)
-                  -> SpatialEdge::PointContainer {
-              insert_unique_edge_point_with_distance_order(edge_points,
-                                                           new_point);
-              return edge_points;
-          });
+    m.def("edge_points_length", &edge_points_length, R"(
+Computes the length of the edge without taking into account the node points.
+It requires the edge_points to be contiguous/ordered.
+    )");
+    m.def("contour_length", &contour_length, R"(
+Computes the contour_length of an edge between two nodes.
+It takes into account the nodes positions.
+It requires the edge_points to be contiguous/ordered.
+    )");
+    m.def(
+            "insert_unique_edge_point_with_distance_order",
+            [](SpatialEdge::PointContainer &edge_points,
+               const SpatialEdge::PointType &new_point)
+                    -> SpatialEdge::PointContainer {
+                insert_unique_edge_point_with_distance_order(edge_points,
+                                                             new_point);
+                return edge_points;
+            },
+            R"(
+Insert point in the input container.
+The input container is a list of points ordered by connectvity, consecutive
+points in the container are connected.
+This computes the distance of the new_point with all the existing
+edge_points.
+
+Parameters:
+----------
+edge_points: [Points]
+    Point container with existing edge_points.
+    This container is not modified, for that, use the returned value.
+    new_edge_points = insert_unique_edge_point_with_distance_order(edge_points, new_point)
+new_point: Point
+    The point to insert in the container.
+          )",
+            py::arg("edge_points"), py::arg("new_point"));
+
+    m.def("check_edge_points_are_contiguous", &check_edge_points_are_contiguous,
+          R"(
+Check edge_points are contiguous.
+It requires the edge_points to be in index space (i.e spacing = [1, 1, 1])
+If compiled in Debug mode it prints the failed points to std::cerr.
+)",
+            py::arg("edge_points"));
 }
