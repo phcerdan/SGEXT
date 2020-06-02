aedaaa4f16f972f814fbc444e42bec04760e163b
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-06-02 12:48:49 +0200

    In force_function_wlc_petrosyan use dynamic_pointer_cast
    Instead of `std::static_pointer_cast` to detect when the bond
    properties are not `BondPropertiesPhysical` containing the neccesary
    persistence_length and kT.
    Before this change, a SEGFAULT was generated, now it throws a `runtime_error`.

--- modules/dynamics/src/force_functions.cpp
+++ modules/dynamics/src/force_functions.cpp
@@ -21,12 +21,13 @@
 #include "force_functions.hpp"
 
 namespace SG {
-ArrayUtilities::Array3D force_function_wlc_petrosyan(
-        const SG::Particle &a, const SG::Particle &b, const SG::Bond &chain) {
+ArrayUtilities::Array3D force_function_wlc_petrosyan(const SG::Particle &a,
+                                                     const SG::Particle &b,
+                                                     const SG::Bond &chain) {
     const auto d_ete_vector = ArrayUtilities::minus(b.pos, a.pos); // F_{a, b}
     const auto d_ete_modulo = ArrayUtilities::norm(d_ete_vector);
     // handle chains with same start/end particles (zero force)
-    if(d_ete_modulo <= 2.0 * std::numeric_limits<double>::epsilon()) {
+    if (d_ete_modulo <= 2.0 * std::numeric_limits<double>::epsilon()) {
         return ArrayUtilities::Array3D();
     }
     const auto &l_contour_length =
@@ -35,16 +36,37 @@ ArrayUtilities::Array3D force_function_wlc_petrosyan(
     // TODO handle relative_extension ~ 1 (wlc_petrosyan_normalized
     // would diverge)
     // TODO: THis is a hack, if relative extension is close to one return zero.
-    // Most of the close-to-one relative extension comes from really short edges.
-    if(relative_extension > 0.97) {
+    // Most of the close-to-one relative extension comes from really short
+    // edges.
+    if (relative_extension > 0.97) {
         return ArrayUtilities::Array3D();
     }
     const auto bond_properties_physical =
-            std::static_pointer_cast<SG::BondPropertiesPhysical>(chain.properties);
+            std::dynamic_pointer_cast<SG::BondPropertiesPhysical>(
+                    chain.properties);
+    if (!bond_properties_physical) {
+        throw std::runtime_error(
+                "To use force_extension_wlc_petrosyan, the bonds need to have "
+                "properties of type BondPropertiesPhysical with "
+                "persistence_length and kT populated");
+    }
+    const auto persistence_length =
+            bond_properties_physical->persistence_length;
+    const auto kT = bond_properties_physical->kT;
+
+    if (persistence_length < std::numeric_limits<double>::epsilon()) {
+        throw std::runtime_error(
+                "persistence_length is zero. Set the bond_physical_properties "
+                "with persistence_length before using this force.");
+    }
+
+    if (kT < std::numeric_limits<double>::epsilon()) {
+        throw std::runtime_error("kT is zero. Set the bond_physical_properties "
+                                 "with kT before using this force.");
+    }
+
     const auto force_modulo = SG::force_extension_wlc_petrosyan(
-            d_ete_modulo, l_contour_length,
-            bond_properties_physical->persistence_length,
-            bond_properties_physical->kT);
+            d_ete_modulo, l_contour_length, persistence_length, kT);
     // d_ete_vector/d_ete_modulo is the unitary vector, in the direction
     // F_{a,b}
     return ArrayUtilities::product_scalar(d_ete_vector,
