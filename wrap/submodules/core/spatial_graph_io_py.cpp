b4d1f8e5787257e46f78e396550e79dc8b61b8d4
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-06-02 12:49:52 +0200

    Python: Provide `py::arg` to wrapped function in `spatial_grap_io`

--- wrap/submodules/core/spatial_graph_io_py.cpp
+++ wrap/submodules/core/spatial_graph_io_py.cpp
@@ -9,16 +9,16 @@ void init_spatial_graph_io(py::module &m) {
     auto mio = m.def_submodule("io");
     mio.def("read_graphviz_sg", [](const std::string &input_file) {
         return SG::read_graphviz_sg(input_file);
-    });
+    }, py::arg("filename"));
     mio.def("read_serialized_sg", [](const std::string &input_file) {
         return SG::read_serialized_sg(input_file);
-    });
+    }, py::arg("filename"));
     mio.def("write_graphviz_sg",
-            [](const std::string &input_file, GraphType &graph) {
-                return SG::write_graphviz_sg(input_file, graph);
-            });
+            [](const std::string &output_file, GraphType &graph) {
+                return SG::write_graphviz_sg(output_file, graph);
+    }, py::arg("filename"), py::arg("graph"));
     mio.def("write_serialized_sg",
-            [](const std::string &input_file, GraphType &graph) {
-                return SG::write_serialized_sg(input_file, graph);
-            });
+            [](const std::string &output_file, GraphType &graph) {
+                return SG::write_serialized_sg(output_file, graph);
+    }, py::arg("filename"), py::arg("graph"));
 }
