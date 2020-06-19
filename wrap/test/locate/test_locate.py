aee4019e5d403c7028afb2c4303464451d2b12cb
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-06-19 19:19:27 +0200

    Python: Locate, wrap graph_points_locator
    Includes wrapping for vtkOctreePointLocator and vtkIdList

--- wrap/test/locate/test_locate.py
+++ wrap/test/locate/test_locate.py
@@ -28,13 +28,60 @@ class TestGetVtkPointsFromGraph(unittest.TestCase):
         cls.graph = fixtures.six_nodes().graph
 
     def test_get_vtk_points_from_graph(self):
-        [vtk_points, pg_map] = locate.get_vtk_points_from_graph(self.graph)
+        print("test_get_vtk_points_from_graph")
+        [vtk_points, id_map] = locate.get_vtk_points_from_graph(self.graph)
         print(vtk_points)
         self.assertEqual(vtk_points.get_number_of_points(), 9)
 
     def test_get_vtk_points_from_graphs(self):
+        print("test_get_vtk_points_from_graphs")
         graphs = list([self.graph, self.graph])
-        [vtk_merge_points, pg_map] = locate.get_vtk_points_from_graphs(graphs)
+        [vtk_merge_points, id_map] = locate.get_vtk_points_from_graphs(graphs)
         print(vtk_merge_points)
 
+class TestGraphPointsLocator(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.graph = fixtures.six_nodes().graph
 
+    def test_id_with_graph_descriptor(self):
+        print("test_id_with_graph_descriptor")
+        id_with_desc = locate.id_with_graph_descriptor()
+        self.assertFalse(id_with_desc.exist)
+
+    def test_build_octree_locator(self):
+        print("test_build_octree_locator")
+        [vtk_points, id_map] = locate.get_vtk_points_from_graph(self.graph)
+        octree_locator = locate.build_octree_locator(vtk_points)
+        print(octree_locator)
+
+    def test_graph_closest_points_by_radius_locator(self):
+        print("test_graph_closest_points_by_radius_locator")
+        [vtk_points, id_map] = locate.get_vtk_points_from_graph(self.graph)
+        octree_locator = locate.build_octree_locator(vtk_points)
+        query_point = [1.0, 1.05, 0]
+        radius = 0.2
+        id_list = locate.graph_closest_points_by_radius_locator(
+            query_point=query_point,
+            octree=octree_locator,
+            id_map=id_map,
+            radius=radius)
+        self.assertEqual(id_list.get_number_of_ids(), 1)
+        graph_id = id_list.get_id(0)
+        self.assertEqual(id_map[graph_id][0].vertex_d, 1)
+
+    def test_graph_closest_n_points_locator(self):
+        print("test_graph_closest_n_points_locator")
+        [vtk_points, id_map] = locate.get_vtk_points_from_graph(self.graph)
+        octree_locator = locate.build_octree_locator(vtk_points)
+        query_point = [1.0, 1.05, 0]
+        number_of_points = 2
+        id_list = locate.graph_closest_n_points_locator(
+            query_point=query_point,
+            octree=octree_locator,
+            id_map=id_map,
+            number_of_points=number_of_points)
+        self.assertEqual(id_list.get_number_of_ids(), number_of_points)
+        # Get the closest of the number_of_points (sorted)
+        graph_id = id_list.get_id(0)
+        self.assertEqual(id_map[graph_id][0].vertex_d, 1)
