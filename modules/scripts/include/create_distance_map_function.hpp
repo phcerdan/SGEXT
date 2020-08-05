09e4c3581ed6e068964120fa0f4b34d6022987b9
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-08-05 11:39:41 +0200

    Docs: Add comment about ITK distance maps
    in create_distance_map_function
    
    Also applies ClangFormat to the file

--- modules/scripts/include/create_distance_map_function.hpp
+++ modules/scripts/include/create_distance_map_function.hpp
@@ -21,33 +21,39 @@
 #ifndef CREATE_DISTANCE_MAP_FUNCTION_HPP
 #define CREATE_DISTANCE_MAP_FUNCTION_HPP
 
-#include <string>
 #include "scripts_types.hpp"
+#include <string>
 
 namespace SG {
 /**
  * Create a distance map image from a binary image
- * A distance map shows the distances of voxel to their nearest background voxel,
- * i.e distance to nearest border.
+ * A distance map shows the distances of voxel to their nearest background
+ * voxel, i.e distance to nearest border.
+ *
+ * DGtal works with voxels of the same size, and it is the most precise.
+ *
+ * If using ITK, take into account that the distance map is eroded, not sure why
+ * (tested with SignedDanielssonDistanceMapImageFilter,
+ * SignedMaurerDistanceMapImageFilter and FastChamferDistanceImageFilter. The
+ * good thing about ITK is that it takes into account spacing. However, if the
+ * image Spacing is isotropic (the voxels are cubes) we can recover the distance
+ * in physical points from the DGtal version.
  *
  * @param input_img input binary image
  * @param verbose verbosity
  *
  * @return distance map image
  */
-typename FloatImageType::Pointer create_distance_map_function(
-        const typename BinaryImageType::Pointer & input_img,
-        const bool use_itk_approximate = false,
-        const bool verbose = false
-        );
+typename FloatImageType::Pointer
+create_distance_map_function(const typename BinaryImageType::Pointer &input_img,
+                             const bool use_itk_approximate = false,
+                             const bool verbose = false);
 typename FloatImageType::Pointer create_distance_map_function_with_dgtal(
-        const typename BinaryImageType::Pointer & input_img,
-        const bool verbose = false
-        );
+        const typename BinaryImageType::Pointer &input_img,
+        const bool verbose = false);
 typename FloatImageType::Pointer create_distance_map_function_with_itk(
-        const typename BinaryImageType::Pointer & input_img,
-        const bool verbose = false
-        );
+        const typename BinaryImageType::Pointer &input_img,
+        const bool verbose = false);
 
 /**
  * @create_distance_map_function but using filenames as inputs/outputs
@@ -59,13 +65,12 @@ typename FloatImageType::Pointer create_distance_map_function_with_itk(
  * Also ITK computes the distance map taking into account spacing of the image.
  * @param verbose verbosity
  */
-typename FloatImageType::Pointer create_distance_map_function_io(
-        const std::string & input_img_file,
-        const std::string & outputFolder,
-        const std::string & foreground = "white",
-        const bool use_itk_approximate = false,
-        const bool verbose = false
-        );
+typename FloatImageType::Pointer
+create_distance_map_function_io(const std::string &input_img_file,
+                                const std::string &outputFolder,
+                                const std::string &foreground = "white",
+                                const bool use_itk_approximate = false,
+                                const bool verbose = false);
 
-} // end ns
+} // namespace SG
 #endif
