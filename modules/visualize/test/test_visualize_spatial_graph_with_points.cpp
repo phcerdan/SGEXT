484fd330134dbd6b7d5cdf4fb1348a7c68c5309c
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2020-09-08 12:05:40 +0200

    CMake: Add VTK9 compatibility
    _modern_VTK_available is ON if at least 8.9
    
    Problems with vtk_module_autoinit on tests.
    autoinit problems are shown at runtime only.

--- modules/visualize/test/test_visualize_spatial_graph_with_points.cpp
+++ modules/visualize/test/test_visualize_spatial_graph_with_points.cpp
@@ -6,6 +6,10 @@
 #include "test_fixtures.hpp"
 #include "get_vtk_points_from_graph.hpp"
 
+// TODO Workaround vtk_module_autoinit not working for test files.
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+
 TEST_F(sg_3D, visualize_spatial_graph_with_points) {
     auto points_map_pair = SG::get_vtk_points_from_graph(g);
     SG::visualize_spatial_graph_with_points(g, points_map_pair.first);
