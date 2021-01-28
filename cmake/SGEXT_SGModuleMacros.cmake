5e2cada75770e981cd6b9c7c519648d1369bc2fc
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2021-01-28 19:59:53 +0100

    Fix python test for windows and add macro SG_add_python_tests
    Setting the environment variable PYTHONPATH was failing in windows, so
    all python test failed to find sgext.
    
    Fix it using the windows path separator ";" or the unix ":",
    depending on platform

--- cmake/SGEXT_SGModuleMacros.cmake
+++ cmake/SGEXT_SGModuleMacros.cmake
@@ -29,3 +29,35 @@ macro(SG_add_gtests)
     endif()
   endforeach()
 endmacro(SG_add_gtests)
+
+# Minimum requirement is a list of python_tests_:
+#  set(python_tests_
+#   test_array3d.py
+#   test_graph.py
+#  )
+# And the module_name_ and test_folder:
+#  get_filename_component(module_name_ ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+#  set(test_folder "${CMAKE_CURRENT_SOURCE_DIR}")
+macro(SG_add_python_tests)
+  # test files should start with "test_"
+  # unittest functions (in .py) should start with "test_" for discover to work
+  foreach(python_test ${python_tests})
+    set(python_test_name_ python||${module_name}||${python_test})
+    add_test(NAME ${python_test_name_}
+      COMMAND
+      ${PYTHON_EXECUTABLE}
+      -m unittest discover
+      -s ${test_folder}
+      -p ${python_test}
+      )
+    # Append the build folder where python lib is to PYTHONPATH
+    # Allowing ctest -R "a_python_test_name_" -V to work
+    if(WIN32)
+      set(_path_sep "\;")
+    else()
+      set(_path_sep ":")
+    endif()
+    set_tests_properties(${python_test_name_} PROPERTIES
+      ENVIRONMENT "PYTHONPATH=$ENV{PYTHONPATH}${_path_sep}${CMAKE_BUILD_PYTHONLIBDIR}/..")
+  endforeach()
+endmacro(SG_add_python_tests)
